35a7da4ccacd3443cbda8fee973115fa92fb19a1
Aalok-Shree-Rijal <holyshot195@gmail.com>
2026-03-18 22:44:04 +0545

    secure api via cloudflare worker

--- mom-proxy/src/index.ts
+++ mom-proxy/src/index.ts
@@ -2,11 +2,11 @@ export default {
   async fetch(request: Request, env: any): Promise<Response> {
     
     const headers = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Content-Type': 'application/json'
-    };
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Content-Type': 'application/json'
+  };
 
     if (request.method === 'OPTIONS') {
       return new Response(null, { headers });
